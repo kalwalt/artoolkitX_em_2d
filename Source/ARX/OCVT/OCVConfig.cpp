0ecd49bde0718ccdd4ae323a25784a3ceec107c4
Thorsten Bux <thorsten.bux@outlook.com>
2019-03-07 14:14:51 +1300

    Keep using AKAZE

--- Source/ARX/OCVT/OCVConfig.cpp
+++ Source/ARX/OCVT/OCVConfig.cpp
@@ -46,7 +46,7 @@ int maxNumberOfToTrack = 1;
 int searchRadius = 15;
 int match_method = cv::TM_SQDIFF_NORMED;
 int featureDetectPyramidLevel = 2;
-int defaultDetectorType = 1;  // iOS Safari can't work with detectorType=1
+int defaultDetectorType = 0;  // detector type only works if compiled with -s DISABLE_EXCEPTION_CATCHING=0 ->I guess OpenCL instantiation throws an exception.
 const double nn_match_ratio = 0.8f; // Nearest-neighbour matching ratio
 const double ransac_thresh = 2.5f; // RANSAC inlier threshold
 cv::RNG rng( 0xFFFFFFFF );
