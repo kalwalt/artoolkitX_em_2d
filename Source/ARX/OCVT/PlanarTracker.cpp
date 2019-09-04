108358c7a774fa62c5cc9b9664406c1d947a49ae
kalwalt <github@kalwaltart.it>
2019-09-04 10:26:17 +0200

    better ProcessFrameData

--- Source/ARX/OCVT/PlanarTracker.cpp
+++ Source/ARX/OCVT/PlanarTracker.cpp
@@ -409,13 +409,12 @@ public:
 
     void ProcessFrameData(unsigned char * frame)
     {
-        cv::Mat data(_frameSizeY, _frameSizeX, CV_8UC4, frame);
         // When using emscripten the image comes in as RGB image from the browser
         // Convert it to Gray
         #if ARX_TARGET_PLATFORM_EMSCRIPTEN
-          cv::UMat colorFrame = data.getUMat(cv::ACCESS_READ);
+          cv::Mat colorFrame(_frameSizeY, _frameSizeX, CV_8UC4, frame);
           cv::UMat grayFrame(_frameSizeY, _frameSizeX, CV_8UC1);
-          cv::cvtColor(colorFrame, grayFrame, cv::COLOR_RGBA2GRAY);
+          cv::cvtColor(colorFrame.getUMat(cv::ACCESS_RW), grayFrame, cv::COLOR_RGBA2GRAY);
           ProcessFrame(grayFrame);
           grayFrame.release();
         #else
